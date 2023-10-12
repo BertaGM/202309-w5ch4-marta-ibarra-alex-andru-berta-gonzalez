860a0be0af0089f3c6eb3d9f10f20dee550a17c1
Alex Andru <alex007d@gmail.com>
2023-10-12 21:20:52 +0200

    Refactor CodersArray

--- app/CodersArray/CodersArray.js
+++ app/CodersArray/CodersArray.js
@@ -1,5 +1,17 @@
 class CodersArray {
-  constructor(...args) {
-    this.values = args;
+  length;
+  data;
+
+  constructor(...coderArguments) {
+    console.log(coderArguments);
+    this.length = 0;
+    this.data = {};
+
+    for (let index = 0; index < coderArguments.length; index++) {
+      this.data[index] = coderArguments[index];
+      this.length += 1;
+    }
   }
 }
+
+export default CodersArray;
