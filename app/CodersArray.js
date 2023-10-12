fd6dc961e135ff771504f988bce91d69a639645e
Berta González <bertagonzalezmartin@gmail.com>
2023-10-12 22:46:57 +0200

    Add filter method at CodersArray class

--- app/CodersArray.js
+++ app/CodersArray.js
@@ -4,6 +4,18 @@ class CodersArray {
   constructor(...elements) {
     this.elements = elements;
   }
+
+  length() {
+    return this.elements.length;
+  }
+
+  textFilter(maxCharacterText) {
+    for (let i = 0; i < this.elements.length; i++) {
+      if (maxCharacterText(this.elements[i])) {
+        console.log(this.elements[i]);
+      }
+    }
+  }
 }
 
 export default CodersArray;
--- app/index.js
+++ app/index.js
@@ -2,3 +2,6 @@ import CodersArray from "./CodersArray.js";
 
 const names = new CodersArray("Marta", "Andru", "Berta");
 const numbers = new CodersArray(1, 2, 3, 4);
+
+console.log(names.textFilter((name) => name === 5));
+console.log(names.length());
